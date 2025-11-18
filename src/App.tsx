d6e7b8282e1d74c2bec294ea258483e3d5286b1a
ramirolacci <ramiroalejandrolacci19@gmail.com>
2025-11-18 11:29:55 -0300

    Smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-black relative overflow-hidden">
+    <div className="min-h-screen bg-black relative">
       {/* Fondo con la imagen: debajo de overlays y contenido */}
       <div className="absolute inset-0 z-0" style={{
         backgroundImage: "url('/background-text.jpg')",
